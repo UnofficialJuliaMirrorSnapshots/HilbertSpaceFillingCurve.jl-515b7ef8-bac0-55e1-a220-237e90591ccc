349f2160269ba35d57e2b5d0626b0f73edfb0067
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 22:07:43 +0000

    Snapshot of branch master taken on 2019-04-07T18:07:42.393-04:00 from "https://github.com/UnofficialJuliaMirror/HilbertSpaceFillingCurve.jl-515b7ef8-bac0-55e1-a220-237e90591ccc"

---
+++ deps/build.jl
@@ -0,0 +1,21 @@
+using BinDeps
+using Compat
+
+@BinDeps.setup
+
+libhilbert = library_dependency("libhilbert")
+
+builddir = joinpath(@__DIR__,"..","src","lib")
+
+provides(SimpleBuild,
+    (@build_steps begin
+        ChangeDirectory( builddir )
+        @build_steps begin
+            `make`
+        end
+    end), 
+libhilbert, installed_libpath=builddir)
+
+@BinDeps.install Dict(
+    "libhilbert" => "libhilbert",
+)
